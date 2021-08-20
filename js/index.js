89617fd4079c252b7524361f0f369b7fdb30a0e3
ArtemSemenistyy <searal1709@gmail.com>
2021-08-20 08:19:41 +0300

    hero + about us

--- js/index.js
+++ js/index.js
@@ -16,4 +16,11 @@ document.addEventListener('DOMContentLoaded', function () {
       document.querySelector(`[data-path="${path}"]`).classList.add('header-bottom__btn-active');
     });
   });
+
+  window.addEventListener('click', e => { // при клике в любом месте окна браузера
+    const target = e.target // находим элемент, на котором был клик
+    if (!target.closest('.header-bottom__dropdown') && !target.closest('.header-bottom__btn')) { // если этот элемент или его родительские элементы не окно навигации и не кнопка
+        document.querySelector('.header-bottom__dropdown').classList.remove('header-bottom__dropdown-active') // то закрываем окно навигации, удаляя активный класс
+    };
+  });
 });
