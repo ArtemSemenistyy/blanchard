3d4a8d5b21c9dc64ecfe34c304330d677042a410
ArtemSemenistyy <searal1709@gmail.com>
2021-08-21 15:20:21 +0300

    gallery

--- js/index.js
+++ js/index.js
@@ -1,29 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-  // document.querySelectorAll('.header-bottom__btn').forEach(function (tabsBtn) {
-  //   tabsBtn.addEventListener('click', function (event) {
-  //     const path = event.currentTarget.dataset.path;
-
-  //     document.querySelectorAll('.header-bottom__dropdown').forEach(function (tabContent) {
-  //       tabContent.classList.remove('header-bottom__dropdown-active');
-  //     });
-
-  //     document.querySelector(`[data-target="${path}"]`).classList.add('header-bottom__dropdown-active');
-
-  //     document.querySelectorAll('.header-bottom__btn').forEach(function (tabContent) {
-  //       tabContent.classList.remove('header-bottom__btn-active');
-  //     });
-
-  //     document.querySelector(`[data-path="${path}"]`).classList.add('header-bottom__btn-active');
-  //   });
-  // });
-
-  // window.addEventListener('click', e => { // при клике в любом месте окна браузера
-  //   const target = e.target // находим элемент, на котором был клик
-  //   if (!target.closest('.header-bottom__dropdown') && !target.closest('.header-bottom__btn')) { // если этот элемент или его родительские элементы не окно навигации и не кнопка
-  //       document.querySelector('.header-bottom__dropdown').classList.remove('header-bottom__dropdown-active') // то закрываем окно навигации, удаляя активный класс
-  //   };
-  // });
-
   const dropdownButtons = document.querySelectorAll('.header-bottom__btn');
   const dropdownMenus = document.querySelectorAll('.header__dropdown-list');
 
@@ -46,5 +21,27 @@ document.addEventListener('DOMContentLoaded', function () {
       dropdownButtons.forEach(el => { el.classList.remove(('header-bottom__btn-active')) });
       dropdownMenus.forEach(el => { el.classList.remove(('dropdown-active')) })
     }
+  });
+
+  const element = document.querySelector('select');
+  const choices = new Choices(element, {
+    searchEnabled: false,
+    itemSelectText: '',
+  });
+
+  var swiper = new Swiper(".section-gallery__swiper", {
+    slidesPerView: 3,
+    slidesPerColumn: 2,
+    slidesPerGroup: 3,
+    spaceBetween: 50,
+    allowTouchMove: false,
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
   });
 });
